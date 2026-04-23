2a77f520da268982830df7ed30e7b2e8a19900f4
yonghwan <qsxcfr123@gmail.com>
2026-04-23 13:19:20 +0900

    [feat] 학생 메인 화면 UI 구현 및 버튼 레이아웃 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: const HomeScreen(),
-      debugShowCheckedModeBanner:false,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('학생 메인'),
-      ),
+      appBar: AppBar(title: const Text('학생 메인')),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+
           children: [
-            const Text('김가별')
+            const SizedBox(height: 20),
+            const Text(
+              '김가별',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 60),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _menuButton(Icons.assignment, '숙제', () {}),
+                _menuButton(Icons.calendar_today, '시간표', () {}),
+                _menuButton(Icons.campaign, '공지', () {}),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _menuButton(IconData icon, String label, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: () {
+        print(label);
+      },
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.blue.shade100,
+            child: Icon(icon, size: 30, color: Colors.blue),
+          ),
+          const SizedBox(height: 8),
+          Text(label),
+        ],
       ),
     );
   }
